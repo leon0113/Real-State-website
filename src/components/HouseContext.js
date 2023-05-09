12209c1f6752a6b10bd41eef4847b9e0bdfcdb21
Leon <tahjib.leon399@gmail.com>
2023-05-10 04:02:45 +0600

    context set

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { housesData } from '../data'
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState('Location (any)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('Property type (any)');
+  const [propertise, setPropertise] = useState([]);
+  const [price, setPrice] = useState('Price range (any)');
+  const [loading, setloading] = useState(false);
+
+  return (
+    <HouseContext.Provider value={{
+      country,
+      setCountry,
+      countries,
+      property,
+      setProperty,
+      propertise,
+      price,
+      setPrice,
+      houses,
+      loading
+    }}>
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
