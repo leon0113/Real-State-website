1917ea7cc95bda40d7d79077d46ab787a4ed4858
Leon <tahjib.leon399@gmail.com>
2023-05-14 01:42:31 +0600

    house cards design

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,7 +14,7 @@ const Search = () => {
       <CountryDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
-      <button onClick={handleClick} className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg'>
+      <button onClick={() => handleClick()} className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg'>
         <AiOutlineSearch></AiOutlineSearch>
       </button>
     </div>
